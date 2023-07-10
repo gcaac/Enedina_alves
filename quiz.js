d752d2b27bf28c560220ac4dbf8272c4b05d8c57
Giulia Carvalho Austregésilo Alves Coelho <48035122+gcaac@users.noreply.github.com>
2023-07-09 21:18:41 -0300

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,27 @@
+export default [
+    {
+      question: "Em que cidade Enedina se formou?",
+      answers: [
+        { option: "Recife", correct: false },
+        { option: "Curitiba", correct: true },
+        { option: "Maceió", correct: false },
+      ],
+    },
+    {
+      question: "Qual era o nome do marido de Enedina?",
+      answers: [
+        { option: "Paulo Marques", correct: false },
+        { option: "Major Domingos", correct: false },
+        { option: "Ela nunca se casou", correct: true },
+      ],
+    },
+    {
+      question: "Quando Enedina se formou?",
+      answers: [
+        { option: "1940", correct: false },
+        { option: "1943", correct: false },
+        { option: "1945", correct: true },
+      ],
+    },
+  ];
+  
